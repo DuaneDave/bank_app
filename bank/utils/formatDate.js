451f6e8652394efeb984024b302b670a53204868
duanedave <duanedave17@gmail.com>
2023-12-05 17:48:10 +0100

    Add formatDate utility function

---
+++ bank/utils/formatDate.js
@@ -0,0 +1,26 @@
+function formatDate(payload) {
+  const date = new Date(payload);
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month}, ${year}`;
+}
+
+export default formatDate;
